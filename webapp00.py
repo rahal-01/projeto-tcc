2ad800d137100f8a22cfd93552b3f27f8752735f
Massaki Igarashi <154282630+igarashimassaki@users.noreply.github.com>
2024-11-07 00:26:57 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,24 +1,33 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
-  
+import requests
+from io import BytesIO
+import pandas as pd
+
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
+st.title("Dashboard Teste")
+st.header("Grupo Murilo & Colegas")
 
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Meu prof. Massaki é D+ e muito convencido!")
+urlCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQruiuZivFX_wDz-2aFue5__5Wu0ua6gZHU77lNNEUb2IUhx0MExIKzvfjBFFDUP-awXC8rKoX7LXEQ/pub?output=csv"
+rD = requests.get(urlCSV)
+dataD = rD.content
+db = pd.read_csv(BytesIO(dataD))
+db.columns = ["DataHora", "Field1", "Field2"]
 
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+def remove_str_start_end(s, start, end):
+    resp = s[:start] + s[end + 1:]
+    return float(resp)
 
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+valor1 = []
+valor2 = []
+for i in range(len(db)):
+    valor1.append(remove_str_start_end(db["Field1"][i], 0, 1))
+    valor2.append(float(db["Field2"][i]))
 
-col2, col3 = st.columns(2)
+df = {'TimeStamp': db['DataHora'], 'Valor1': valor1, 'Valor2': valor2}
+df = pd.DataFrame(df)
+st.write(df.head(3))
 
-with col2:
-    st.header("NALA")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("CORUJA")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+st.title("Valores Médios: ")
+st.title(f"Valor 1: {df['Valor1'].mean()}")
+st.title(f"Valor 2: {df['Valor2'].mean()}")
